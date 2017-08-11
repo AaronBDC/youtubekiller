d3c782e7991c5b0882a86db323a80be1a0e3e492
Aaron DC <aaronbdc@mail.com>
2017-08-11 09:29:31 -0400

    working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React , { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -7,33 +8,39 @@ import VideoDetail from './components/video_detail';
 
 
 const API_KEY= "AIzaSyCyXaeG3lixO9UwjnAE5iIjehk3qP9WbjI";
-// create a new component, this component shoud produce
-// some html
+
 class App extends Component {
   constructor(props){
     super(props);
-    this.state = { videos: []}
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      //console.log(data);
-      this.setState({videos});
-      //this.setState({ videos: videos});
-    });
-  }
 
-  // the parent App component is passing props (videos to VideoList [child component])
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('surfboards');
+}
+
+videoSearch(term){
+  YTSearch({key: API_KEY, term: term}, (videos) => {
+    this.setState({
+      videos:videos,
+      selectedVideo: videos[0]
+    });
+  });
+}
+
   render(){
+    const videoSearch =_.debounce((term) => { this.videoSearch(term)}, 300);
     return (
       <div>
-        <SearchBar />
-        <VideoDetail video={this.state.videos[0]} />
-        <VideoList videos= { this.state.videos } />
-
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+          videos= { this.state.videos } />
       </div>
     );
-
   }
 }
-//Take this component's generated HTML and put it
-//on the page ( in the DOM)
-//React.render(App);
 ReactDOM.render(<App />, document.querySelector('.container'));
